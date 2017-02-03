9966e46d886fd8a102fdf42dc1b26e2ca668b045
yaojianwei <yaojianwei@meicai.cn>
2017-02-03 18:36:44 +0800

    json

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,11 +2,12 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class IndexController {
 
 	@RequestMapping("/index")
-	public String showIndex(){
+	public String showIndex() {
 		return "index";
 	}
 }
