dd6eac1d9d4668bbcfd2fe55473070e56d567c3f
yaojianwei <yaojianwei@meicai.cn>
2017-02-13 16:57:37 +0800

    搜索 solr

---
+++ taotao-search/src/main/java/com/taotao/search/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.search.service;
+
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.search.pojo.SearchResult;
+
+public interface ItemService {
+
+	public TaotaoResult importAllItems();
+
+}
